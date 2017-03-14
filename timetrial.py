ef3a9dd8747633a1e8e49dc5344e57e5afb82bcd
Steve Gledhill <stevegleds@users.noreply.github.com>
2017-03-14 21:04:44 +0000

    14 March 2017 Added code to select brief or full csv Also added option to include full excel. Installed xlrd to be able to read from xls file Trying to sort pbs by runner to see if it is working correctly Working on jupyter noteboook because this is best way to work with pandas for testing. Takes time to load. Need to look at later.

--- timetrial.py
+++ timetrial.py
@@ -1,12 +1,26 @@
 import pandas as pd
 from readwrite import process_sourcefile
 
+response = input('Use Brief csv (B) or Full (F) csv file? or complete Excel file (X)')
+if response.lower() == 'f':
+    sourcefile = 'timetrialbrief.csv'
+elif response.lower() == 'f':
+    sourcefile = 'timetrialfull.csv'
+else:
+    sourcefile = 'errraces.xlsm'
+print("We are using file: ", sourcefile)
+
 try:
-    print(process_sourcefile('timetrial.csv'))
-    df = pd.read_csv('timetrial.csv')  # creates df with header conveniently inferred by default
-    print(df)
+    print(process_sourcefile(sourcefile))
+    # df = pd.read_csv(sourcefile)  # creates df with header conveniently inferred by default
+    print("Reading file ... please wait")
 except:
     print("The file is either out of date or not found. Please save an up to date file to this folder")
+
+if response.lower() == 'x':
+    df = pd.read_excel(sourcefile, sheetname='Timetrialresults', names=['Date', 'Runner', 'Time', 'Pace', 'Digitime'])
+else:
+    df = pd.read_csv(sourcefile)  # creates df with header conveniently inferred by default
 
 def add_runner(runner, runner_list):
     return runner_list.append(runner)
@@ -22,7 +36,7 @@ for index, row in df.iterrows():
 print('List of runners: ', runner_list)
 
 pbs = df.groupby(['Runner'])['Pace'].transform(min) == df['Pace']
-print(df[pbs])
+print(df[pbs].sort_index('Runner'))
 
 
 
