30a4e9368fbab73c5052a46d9a275490f4e2a264
Steve Gledhill <stevegleds@users.noreply.github.com>
2017-04-11 20:25:32 +0100

    11 April 2017 Working with csv '3' created dictionary of runners with the first date they ran : runnerdict This seems now to have created runner objects with correct start dates next step: add initial times to people

--- timetrial.py
+++ timetrial.py
@@ -8,31 +8,6 @@ Then test to see if it is ok and create appropriate pandas datafile
 '''
 folder = os.path.join('','results')
 
-def getfilelist(folder):
-    try:
-        filelist = os.listdir(folder)
-    except:
-        print('No such folder.')
-    filechoice = []
-    for file in sorted(filelist):
-        filechoice.append(file)
-    return filechoice
-
-filelist = getfilelist(folder)
-for file in enumerate(filelist):
-    print(file)
-
-sourcefile = os.path.join(folder, filelist[int(input('Which file do you want to use? '))])
-print("We are using file: ", sourcefile)
-
-try:
-    print(process_sourcefile(sourcefile))
-    # df = pd.read_csv(sourcefile)  # creates df with header conveniently inferred by default
-    print("Reading file ... please wait")
-except:
-    print("The file is either out of date or not found. Please save an up to date file to this folder")
-
-df = pd.read_csv(sourcefile, parse_dates=['Date'], dayfirst=True)  # creates df with header conveniently inferred by default
 
 '''
 Building class to store info.
@@ -47,17 +22,73 @@ class Person:
     def __str__(self):
         return '{} started on {}'.format(self.name, self.created)
 
-# TODO see if this is used anywhere
-def print_data(df):
-    for index, row in df.iterrows():
-        print('These are the runners and their paces for all races:')
-        print(index, row['Runner'], row['Pace'])
 
+# # TODO see if this is used anywhere
+# def print_data(df):
+#     for index, row in df.iterrows():
+#         print('These are the runners and their paces for all races:')
+#         print(index, row['Runner'], row['Pace'])
+
+
+'''
+This section is work in progress to identify pbs
+'''
+# jan2015 = df[df.Date == '2015-01-05']
+# feb2015 = df[df.Date == '2015-02-02']
+# print('The results for January 2015 were: \n', jan2015)
+# print('The results for February 2015 were: \n', feb2015)
+# print('The average pace for January was: ', jan2015['Digitime'].mean(), '\n and for February was: ', feb2015['Digitime'].mean())
+# # print(df[df['Runner'].str.contains('Craig Bingham')])  # prints all Craig's results
+# latest_results = feb2015
+# previous_results = jan2015
+# for runner in runnerdict:
+#     if runner in latest_results.Runner.values and runner in previous_results.Runner.values:
+#         latest_race = latest_results[latest_results['Runner'].str.contains(runner)]
+#         previous_race = previous_results[previous_results['Runner'].str.contains(runner)]
+#         # print(latest_race)
+#         # print(previous_race)
+#         print('Latest time for {} was {} minutes'.format(runner, latest_race['Digitime'].values))
+#         print('Previous time for {} was {} minutes' .format(runner, previous_race['Digitime'].values))
+#         print('{} has improved by {} seconds \n' .format(runner, 60*(previous_race['Digitime'].values - latest_race['Digitime'].values)))
+
+if __name__ == "__main__":
+    print('Welcome to the timetrial results')
+
+
+# Present list of files to the console
+filelist = getfilelist(folder)
+for file in enumerate(filelist):
+    print(file)
+# Ask user to choose their file
+sourcefile = os.path.join(folder, filelist[int(input('Which file do you want to use? '))])
+print("We are using file: ", sourcefile)
+
+try:
+    print(process_sourcefile(sourcefile))
+    # df = pd.read_csv(sourcefile)  # creates df with header conveniently inferred by default
+    print("Reading file ... please wait")
+except:
+    print("The file is either out of date or not found. Please save an up to date file to this folder")
+
+df = pd.read_csv(sourcefile, parse_dates=['Date'],
+                 dayfirst=True)  # creates df with header conveniently inferred by default
+
+df = df.sort_values(by=['Date', 'Runner'], ascending=[True, True])
 '''
 runnerdict will store information that will be used to create person instances
 '''
-runnerdict = dict(zip(df.Runner, df.Date))
-runners = {}
+runnerdict = {}
+print('pandas file is:', df)
+for index, row in df.iterrows():
+    if runnerdict.get(row['Runner']):
+        print('runner exists')
+        pass
+    else:
+        runnerdict[row['Runner']] = row['Date']
+print(len(runnerdict))
+# print(runnerdict)
+print(runnerdict)
+runners = {}  # Empty dict to store runner objects
 for k, v in runnerdict.items():
     runners[k] = Person(k, v)
 print('runner people created')
@@ -69,25 +100,3 @@ byrunner = df.groupby('Runner')
 byrunner['Digitime'].min()
 byrunner['Digitime'].agg([np.min, np.sum, np.mean, len])
 pb = byrunner['Digitime'].transform(min) == df['Digitime']
-
-
-'''
-This section is work in progress to identify pbs
-'''
-jan2015 = df[df.Date == '2015-01-05']
-feb2015 = df[df.Date == '2015-02-02']
-print('The results for January 2015 were: \n', jan2015)
-print('The results for February 2015 were: \n', feb2015)
-print('The average pace for January was: ', jan2015['Digitime'].mean(), '\n and for February was: ', feb2015['Digitime'].mean())
-# print(df[df['Runner'].str.contains('Craig Bingham')])  # prints all Craig's results
-latest_results = feb2015
-previous_results = jan2015
-for runner in runnerdict:
-    if runner in latest_results.Runner.values and runner in previous_results.Runner.values:
-        latest_race = latest_results[latest_results['Runner'].str.contains(runner)]
-        previous_race = previous_results[previous_results['Runner'].str.contains(runner)]
-        # print(latest_race)
-        # print(previous_race)
-        print('Latest time for {} was {} minutes'.format(runner, latest_race['Digitime'].values))
-        print('Previous time for {} was {} minutes' .format(runner, previous_race['Digitime'].values))
-        print('{} has improved by {} seconds \n' .format(runner, 60*(previous_race['Digitime'].values - latest_race['Digitime'].values)))
